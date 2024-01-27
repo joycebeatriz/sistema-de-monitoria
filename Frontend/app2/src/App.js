1af7013e9fb0715d4a1a2f11431e9f1f5c5078d8
joycebeatriz <joycesilva@discente.ufg.br>
2024-01-27 20:32:40 -0300

    primeira versão

---
+++ Frontend/app2/src/App.js
@@ -0,0 +1,42 @@
+// src/App.js
+import React from 'react';
+
+function App() {
+
+  const element = <div>Olá Joyce!</div>;
+
+  //Horários disponíveis para monitoria
+  const horariosDisponiveis = [
+    {dia: 'segunda', horarios: ['8:00', '9:00'], monitor: 'João'},
+    {dia: 'terça', horarios: ['10:00', '11:00'], monitor: 'Luana'},
+    {dia: 'quarta', horarios: ['13:00', '14:00'], monitor: 'Pedro'},
+  ];
+  
+  // Renderiza os horários disponíveis
+
+  const renderHorariosDisponiveis = (horariosDisponiveis) => {
+  return horariosDisponiveis.map((horario, index) => (
+    <div key={index}> 
+      <p><strong>Dia:</strong> {horario.dia}</p>
+      <p><strong>Horários:</strong> {horario.horarios}</p>
+      <p><strong>Monitor:</strong> {horario.monitor}</p>
+      <button onClick = {() => agendaMonitoria(horario)}>Agendar</button>
+    </div>
+  ));
+  };
+  
+  // Agenda monitoria
+
+  const agendaMonitoria = (horario) => {
+    console.log(`Agendou monitoria para o ${horario.dia} às ${horario.horarios} com o monitor ${horario.monitor}`);
+  };
+
+  return (
+    <div>
+      <h1>Agendamento de monitoria</h1>
+      {renderHorariosDisponiveis(horariosDisponiveis)}
+    </div>
+  );
+}
+
+export default App;
